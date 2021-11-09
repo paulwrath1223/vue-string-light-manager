48afe0f908f0975a81374257279ba67be397a094
paulwrath1223 <paul@fornage.net>
2021-11-09 09:45:00 -0800

    in prog, issue at firebase.js line 187

--- src/firebase.js
+++ src/firebase.js
@@ -23,12 +23,27 @@ import {app, globalUser, signIn} from "@/main.js";
 
 function arduinoToJson(arduinoNotJSON)
 {
+
+    function extractKeyFrameIndices(arduinoNotJSON) {
+        let generatedKeyFrameIndices = []
+        let currentIndex = 0;
+        for(const colorNode of arduinoNotJSON.colors)
+        {
+            generatedKeyFrameIndices.push(currentIndex);
+            currentIndex += colorNode.transitionFrames;
+        }
+        return generatedKeyFrameIndices;
+    }
     console.log("function arduinoToJson begun")
     let colorJSON = colorCompile(arduinoNotJSON);
+
+
+
     return({
         "Name": arduinoNotJSON.location,
         "colorLength": colorJSON.length,
         "colors": colorJSON,
+        "keyFrameIndices": extractKeyFrameIndices(arduinoNotJSON),
         "mirrorIndex" : arduinoNotJSON.mirrorIndex,
         "numLights" : arduinoNotJSON.lightsCount,
         "speed" : arduinoNotJSON.speed,
@@ -62,12 +77,13 @@ export function getCurrentUserImage()
 export async function uploadArduino(arduino) //WRITE KEYFRAMEINDICES DUMBASS
 {
 
+
     await verifyUser();
     while(globalUser === null)
     {
         await delay(10);
     }
-    const id = arduino.currentID;
+    const id = arduino.arduinoID;
     const json = arduinoToJson(arduino);
     console.log("JSON to upload to id " + id + ": ");
     console.log(json);
@@ -76,9 +92,24 @@ export async function uploadArduino(arduino) //WRITE KEYFRAMEINDICES DUMBASS
 
 }
 
-function JSONtoHex(currentNodeColor) {
-    return "#000000";
+
+function componentToHex(c) {
+    const hex = Number(c).toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
+
+function JSONtoHex(currentNodeColor) {
+    console.log("JSONtoHex: ");
+    console.log(currentNodeColor);
+
+    const r = currentNodeColor.r;
+    const g = currentNodeColor.g;
+    const b = currentNodeColor.b;
+    const result = ("#" + componentToHex(r) + componentToHex(g) + componentToHex(b));
+    console.log(result);
+    return result;
+}
+
 
 export async function downloadArduino(id)
 {
@@ -102,51 +133,66 @@ export async function downloadArduino(id)
             {color: "#000000", transitionFrames: 4}
         ]
     };
-
+    console.log("flag 1");
     await verifyUser();
     while(globalUser === null)
     {
         await delay(10);
     }
 
-
+    console.log("flag 2");
     arduinoOut.speed = await getAttribute(id, "speed");
     arduinoOut.numLights = await getAttribute(id, "numLights");
     arduinoOut.location = await getAttribute(id, "Name");
+    console.log("flag 3");
     let rawColors = await getAttribute(id, "colors");
-    let keyFrameIndices = await getAttribute(id, "keyFrameIndices");
-    let colorNodes = {
-        "colors":
-            [
-
-            ]
-
-    };
+    console.log("getting keyframe indices: ");
+    let keyFrameIndices = await getAttribute(id, "keyFrameIndices"); //NOT ACTUALLY WAITING
+    console.log(keyFrameIndices);
+    let colorNodes = [];
+    console.log("flag 4");
     const templateColor = {
         "color": "#000000",
         "transitionFrames": 1
     };
     let tempColor = templateColor;
     // let currentNodeNumFrames = 0;
+    console.log("flag 5");
     let currentNodeColor = {
         "r": 0,
         "g": 0,
         "b": 0
     };
+    console.log("about to read length of " + keyFrameIndices);
+    const keyFrameIndicesLength = keyFrameIndices.length;
+    console.log("keyFrameIndicesLength: " + keyFrameIndicesLength);
+    const rcl = rawColors.length;
     for(
         let currentKeyFrameIndexIndex = 0;
-        currentKeyFrameIndexIndex < keyFrameIndices.length;
+        currentKeyFrameIndexIndex < keyFrameIndicesLength;
         currentKeyFrameIndexIndex++
     )
     {
-        const nextIndex = keyFrameIndices[(currentKeyFrameIndexIndex + 1) % keyFrameIndices.length]
+        let nextIndex = rcl;
+        if((currentKeyFrameIndexIndex + 1) !== keyFrameIndicesLength)
+        {
+            nextIndex = keyFrameIndices[currentKeyFrameIndexIndex + 1];
+        }
         currentNodeColor = rawColors[keyFrameIndices[currentKeyFrameIndexIndex]];
-        tempColor.transitionFrames = keyFrameIndices[currentKeyFrameIndexIndex] - nextIndex;
         tempColor = templateColor;
+        tempColor.transitionFrames = nextIndex - keyFrameIndices[currentKeyFrameIndexIndex];
         tempColor.color = JSONtoHex(currentNodeColor);
-        colorNodes.colors.push(tempColor);
+        console.log("tempColor: ");
+        console.log(tempColor);
+        const finalNode = tempColor; //  WHY DOES COLOR NODES CONTAIN 4 INSTANCES OF THE LAST NODE?????
+        console.log("finalNode: ");
+        console.log(finalNode);
+        colorNodes.push({"index": currentKeyFrameIndexIndex,
+            "node": finalNode}); // INDEX WORKS FINE !!!!!!!!!!!!!
     }
-    arduinoOut.colors = colorNodes.colors; // restructure vard
+    console.log("colorNodes: ");
+    console.log(colorNodes);
+    arduinoOut.colors = colorNodes;
     return arduinoOut;
 
 }
@@ -161,15 +207,20 @@ async function verifyUser()
     console.log(globalUser);
 }
 
-async function getAttribute(id, attribute)
-{
+async function getAttribute(id, attribute) {
     const db = getDatabase(app);
-
+    let tempResult = undefined;
     const tempPath = ('Arduinos/' + id + '/' + attribute);
     const tempRef = ref(db, tempPath);
+    console.log("get id " + id + "'s " + attribute);
     onValue(tempRef, (snapshot) => {
-        const tempResult = snapshot.val();
-        console.log("get id " + id + "'s " + attribute+": " + tempResult);
-        return tempResult;
+        tempResult = snapshot.val();
+        console.log("get id " + id + "'s " + attribute + ": " + tempResult);
+
     });
+    while(tempResult === undefined)
+    {
+        await delay(10);
+    }
+    return tempResult;
 }
