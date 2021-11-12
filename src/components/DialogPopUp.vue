d21ed32740fa7b936088bea3bbe145484b2fc35e
Paul <paulwrath1223>
2021-11-11 16:32:20 -0800

    totally broken fucked up

---
+++ src/components/DialogPopUp.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <h1>{{ title }}</h1>
+  <hr />
+  <p>{{ body }}</p>
+</template>
+
+<script>
+export default {
+  name: "DialogPopUp",
+  props: {
+    title: String,
+    body: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
